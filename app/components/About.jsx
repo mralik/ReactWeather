587aba8673a2778bbd4224b1ed8db1274bc1c83a
mralik <mralik2020@gmail.com>
2017-02-20 12:39:12 +0800

    Update About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,8 +4,11 @@ var React = require('react');
 
 var About = (props)=>{
   return (
-      <h3>About </h3>
-      <p>Wellcome to About Page.</p>
+<div>
+  <h3>About</h3>
+  <p>Wellcome to About Page</p>
+</div>
+
   )
 };
 module.exports = About;
